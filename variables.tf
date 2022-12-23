deaa2555811daf88986ea3d7c06c61021e231a3e
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-23 19:51:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -51,19 +51,31 @@ variable "compartment_description" {
 }
 
 
+#networking parameters
+variable "vcn_cidr_blocks" {
+  description = "The list of IPv4 CIDR blocks the VCN will use."
+  default     = ["10.0.0.0/16"]
+  type        = list(string)
+}
 
-variable "subcompartments" {
+variable "vcn_dns_label" {
+  description = "A DNS label for the VCN"
+  type        = string
+}
+
+variable "vcn_name" {
+  type        = string
+  default     = "retail-sydney-vcn"
+}
+
+
+variable "network_compartment" {
+  description = "network compartment"
+  type        = string
+}
+
+
+variable "subnets" {
   type    = any
   default = {}
-}
-
-#network parameters
-variable "subnets" {
-   type        = any
-   default     = {}
-}
-
-#variable "subnet_cidr_block" {}
-
-variable "vcn_id" {
 }
